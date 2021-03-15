e816eb20240a225eab3172fcf14014310737afc8
Joaquin Correa <jecorrea@protonmail.com>
2021-03-15 22:52:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,6 +9,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
 display_board(board)
